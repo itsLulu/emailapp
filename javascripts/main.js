933de9559a939532fd3f6d59c134c97ce061dbd2
itsLulu <itslulu@gmail.com>
2014-07-07 18:33:19 -0400

    add animation
    add animation for icon transform and div transitions

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,14 +1,37 @@
 // type something here
 
 var browserheight = window.innerHeight;
-
-console.log(browserheight);
+var browserwidth = window.innerWidth;
 
 $('#emailbox').height(browserheight);
 
+//get mail box
+$('#nav_mailbox').click(function(){
+	$('#emailbox').toggleClass('slideleft');
+	$('#nav-icon1').toggleClass('rotateicon');
+});
 
-/*get mail box*/
-$('.nav-btn').click(function(){
-	console.log('slide');
+//get mail list
+$('#nav_maillist').click(function(){
+	$('#emaillist').toggleClass('slideleft');
+	$('#nav-icon2').toggleClass('rotateicon');
+});
+
+var emaillist = $('#emaillist');
+	
+//show email content
+$('.emaillist_listitem').click(function(){
+	if(browserwidth <= '630'){
+		emaillist.toggleClass('slideleft');
+	}
+});
+
+//star an email
+$('.star').click(function(){
+	$(this).toggleClass('stared');
+});
+
+//inbox - current selected 
+$('#inbox').click(function(){
 	$('#emailbox').toggleClass('slideleft');
 });
